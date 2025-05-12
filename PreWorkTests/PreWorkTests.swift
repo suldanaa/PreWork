331c3183a4d957ac08b600306f83f340fe1ed687
suldanaa <suldanaafrah@gmail.com>
2025-05-11 19:03:40 -0700

    Initial Commit

---
+++ PreWorkTests/PreWorkTests.swift
@@ -0,0 +1,17 @@
+//
+//  PreWorkTests.swift
+//  PreWorkTests
+//
+//  Created by Suldana Afrah on 5/11/25.
+//
+
+import Testing
+@testable import PreWork
+
+struct PreWorkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
